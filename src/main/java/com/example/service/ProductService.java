5d13648786c718f61bfacb84912dc0933cec0228
RomanMakarevich <romanmak955@gmail.com>
2020-02-17 01:56:46 +0300

    Chenge ProductService

--- src/main/java/com/example/service/ProductService.java
+++ src/main/java/com/example/service/ProductService.java
@@ -2,19 +2,37 @@ package com.example.service;
 
 
 import com.example.dto.ProductDTO;
+import com.example.mapper.ProductMapper;
+import com.example.reposiroty.ProductRepository;
+import lombok.Data;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+@Data
 @Service
 public class ProductService {
-    public List<ProductDTO> init() {
-        return List.of(ProductDTO.builder()
+
+    private final ProductRepository productRepository;
+    private final ProductMapper productMapper;
+
+    public void init() {
+        productRepository.save(productMapper.sourceToDestination(ProductDTO.builder()
                 .productId(1)
                 .productName("keg")
                 .material("sreel")
                 .weight(7.1)
-                .build());
+                .build()));
+
+    }
+
+    public List<ProductDTO> getList(){
+        return productRepository.findAll().stream().map(productMapper::destinationToSource).collect(Collectors.toList());
+    }
+    public Optional<ProductDTO> getProduct(final long id){
+        return productRepository.findById(id).map(productMapper::destinationToSource);
     }
 }
 
