d62d675d517a8b03b61bd151edda2f841c779e39
mak <romanmak955@gmail.com>
2020-02-29 19:54:20 +0300

    Change AddBasketListService

--- src/main/java/com/example/service/AddBasketListService.java
+++ src/main/java/com/example/service/AddBasketListService.java
@@ -1,16 +1,20 @@
 package com.example.service;
 
+import antlr.collections.impl.LList;
 import com.example.entity.BasketEntity;
 import com.example.entity.ProductItemEntity;
 import com.example.mapper.ProductItemMapper;
 import com.example.reposiroty.BasketRepository;
 import com.example.reposiroty.ProductRepository;
 import com.example.reposiroty.UserRepository;
+import liquibase.pro.packaged.E;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
 
 
 import javax.transaction.Transactional;
+import java.util.LinkedList;
 import java.util.List;
 
 @Service
@@ -25,21 +29,37 @@ public class AddBasketListService {
     @Transactional
     public void addBasketList(final long userId, final long productId, final Long numberOfProduct) {
 
+        final LinkedList<ProductItemEntity> basketList = new LinkedList();
+        basketList.add(createProductItem(productId, numberOfProduct));
+
+        if (basketRepository.findByUserId(userId) != null) {
+
+            basketList.addAll(basketRepository.findByUserId(userId).getBasketList());
+            basketList.getFirst().setBasketEntity(basketRepository.findByUserId(userId));
+            basketRepository.findByUserId(userId).setBasketList(basketList);
+
+        } else {
+            BasketEntity basketEntity = new BasketEntity();
+            basketList.getFirst().setBasketEntity(basketRepository.findByUserId(userId));
+            basketEntity.setBasketList(basketList);
+            basketEntity.setUserEntity(userRepository.getOne(userId));
+            basketEntity.setTotalCost(basketEntity.
+                    getTotalCost() + basketEntity.
+                    getBasketList().
+                    stream().
+                    mapToDouble(ProductItemEntity::getCost).
+                    sum());
+
+            basketRepository.save(basketEntity);
+        }
+    }
+
+    private ProductItemEntity createProductItem(final long productId, final Long numberOfProduct) {
         final ProductItemEntity productItemEntity = new ProductItemEntity();
         productItemEntity.setProductEntity(productRepository.getOne(productId));
         productItemEntity.setNumberOfProduct(numberOfProduct);
         productItemEntity.setCost(productItemEntity.getProductEntity().getCost() * productItemEntity.getNumberOfProduct());
 
-        BasketEntity basketEntity = new BasketEntity();
-        basketEntity.setBasketList(List.of(productItemEntity));
-        basketEntity.setUserEntity(userRepository.getOne(userId));
-        basketEntity.setTotalCost(basketEntity.
-                getTotalCost() + basketEntity.
-                getBasketList().
-                stream().
-                mapToDouble(ProductItemEntity::getCost).
-                sum());
-
-        basketRepository.save(basketEntity);
+        return productItemEntity;
     }
 }
